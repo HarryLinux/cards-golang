8c4b11f88d00d79b19c18e7b65850afa8cf31d1d
Harold Fich <haroldfich@gmail.com>
2022-01-05 16:05:20 -0500

    shuffle receiver func

--- deck.go
+++ deck.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -62,4 +64,14 @@ func newDeckFromFile(filename string) deck {
 	}
 
 	return deck(strings.Split(string(byteSlice), ","))
+}
+
+func (d deck) shuffle() {
+	rand.Seed(time.Now().UnixNano())
+
+	for i := range d {
+		newPosition := rand.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
